af33ca76b17f12906686efb7d2a23d00d9c457d2
Ralph Ribeiro <ralph_ribeiro@msn.com>
2021-02-11 16:00:59 -0300

    unpack zip file

--- tests/unit/test_unpack_payload.py
+++ tests/unit/test_unpack_payload.py
@@ -7,9 +7,10 @@ from src.app.payload import unpack_payload
 
 def test_unpack_payload_given_path(temp_dir):
     file_name = 'file.txt'
+    content = b'data test'
     path = join(temp_dir, file_name)
     with open(path, 'wb') as f:
-        f.write(b'data test')
+        f.write(content)
 
     path_zip = join(temp_dir, 'file.zip')
     with ZipFile(path_zip, 'x') as z:
@@ -19,3 +20,5 @@ def test_unpack_payload_given_path(temp_dir):
     assert exists(path_zip)
     unpack_payload(path_zip)
     assert exists(path)
+    with open(path, 'rb') as f:
+        assert f.read() == content
